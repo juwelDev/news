9735dfc1bbe9a59b1a4dce7b6fdfa28a9e6c31a2
najmulhouda <najmulhouda6@gmail.com>
2023-10-15 20:38:31 +0600

    task done

--- app.js
+++ app.js
@@ -4,13 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var session = require('express-session');
-var passport = require('passport');
-var device = require('express-device');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var dashboardRouter = require('./routes/dashboard');
 
 var app = express();
 
@@ -23,37 +18,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('express-useragent').express())
-app.use(device.capture());
-
-// Handle File upload
-app.use(session({
-  secret: 'secret',
-  saveUninitialized: true,
-  resave: true
-}));
-
-// Passport 
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(passport.authenticate('session'));
-app.use(require('connect-flash')());
-app.use(function (req, res, next) {
-  res.locals.messages = require('express-messages')(req, res);
-  next();
-});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/dashboard', dashboardRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ models/userModel.js
@@ -0,0 +1,67 @@
+var mongoosedb = require("../config/dbconfig");
+var bcrypt = require("bcryptjs");
+
+let date = new Date();
+date = date.toLocaleDateString("en-us", {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+  hour: "numeric",
+  minute: "numeric",
+  hour12: true,
+});
+
+// User Schema
+var UserSchema = mongoosedb.Schema({
+  username: {
+    type: String,
+    index: true,
+  },
+  password: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  name: {
+    type: String,
+  },
+  profileimg: {
+    type: String,
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+  date_at: {
+    type: String,
+    default: date,
+  },
+});
+
+var UserModel = (module.exports = mongoosedb.model("UserModel", UserSchema));
+
+// module.exports.getUserById = function (id, callback) {
+//   UserModel.findById(id, callback);
+// };
+
+// module.exports.getUserByUsername = function (username, callback) {
+//   var query = { username: username };
+//   UserModel.findOne(query, callback);
+// };
+
+module.exports.comparePassword = function (candidatePassword, hash, callback) {
+  bcrypt.compare(candidatePassword, hash, function (err, isMatch) {
+    callback(null, isMatch);
+  });
+};
+
+module.exports.createUser = function (newUser, callback) {
+  bcrypt.genSalt(10, function (err, salt) {
+    bcrypt.hash(newUser.password, salt, function (err, hash) {
+      newUser.password = hash;
+      newUser.save(callback());
+    });
+  });
+};
